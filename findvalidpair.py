ed284bf1f7780a1b2e3afc77f1d9b4b47a9ff4a8
bytephilosopher <aberayostina@gmail.com>
2026-02-12 09:11:17 +0300

    find the valid pair of string based on the condition

---
+++ findvalidpair.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+class Solution:
+    def findValidPair(self, s: str) -> str:
+        freq = Counter(s)
+        
+        for i in range(len(s) - 1):
+            a, b = s[i], s[i + 1]
+            
+            if a != b and freq[a] == int(a) and freq[b] == int(b):
+                return a + b
+        
+        return ""
