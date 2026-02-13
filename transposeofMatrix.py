ae16dbd5236aa9742fec491755ea495bf13d9933
bytephilosopher <aberayostina@gmail.com>
2026-02-13 15:05:06 +0300

    transpose of matrix

---
+++ transposeofMatrix.py
@@ -0,0 +1,11 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        row=len(matrix)
+        col=len(matrix[0])
+        newmat=[[0]*row for _ in range(col)]
+
+        for r in range(row):
+            for c in range(col):
+                newmat[c][r]=matrix[r][c]
+        return newmat
+        
