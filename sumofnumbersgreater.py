06b492a9b904197e8ba0e035a8bdecb4e4e19a32
bytephilosopher <aberayostina@gmail.com>
2026-02-19 18:05:36 +0300

    find the huo many numbers are greater than the given number in list

---
+++ sumofnumbersgreater.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def smallerNumbersThanCurrent(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans=[]
+        count=0
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i]>nums[j]:
+                    count+=1
+            ans.append(count)
+            count=0
+        return ans
+            
