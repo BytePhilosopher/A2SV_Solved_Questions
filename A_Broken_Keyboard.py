421a2e9820bdce05ba53bac632aeec931a775099
bytephilosopher <aberayostina@gmail.com>
2026-02-25 18:39:22 +0300

    find the malfucntional letter

--- A_Broken_Keyboard.py
+++ A_Broken_Keyboard.py
@@ -1,14 +1,20 @@
-from collections import Counter
-
 
 t=int(input())
 for _ in range(t):
     s = input()
-    count=Counter(s)
-    res=[]
+    ans=set()
 
-    for key,val in count.items():
-        if val%2!=0:
-           res.append(key)
-    res.sort()
-    print("".join(res))
+    ptr=0
+    while ptr<len(s):
+        i=ptr
+        check=0
+        while i<len(s) and s[i]==s[ptr]:
+            check+=1
+            i+=1
+        if check%2!=0:
+            ans.add(s[ptr])
+        ptr=i
+    ans=sorted(ans)
+
+    print("".join(ans))
+
