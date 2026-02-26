a2c4504eb10e42b19d895d472afbb0aed5c9daa4
Yostina Abera Gobie <aberayostina@gmail.com>
2026-02-26 20:30:35 +0300

    Time: 144 ms (30.75%), Space: 19.7 MB (44.52%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,14 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+        hash={}
+        l=0
+        res=0
+        for r in range(len(s)):
+            hash[s[r]]=1+ hash.get(s[r],0)
+            while (r-l+1)-max(hash.values())>k:
+                hash[s[l]]-=1
+                l+=1
+            res=max(res,r-l+1)
+
+        return res
