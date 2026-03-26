5e2427ea44b0003bc5390c16560a1bb2f42ebffe
Yostina Abera Gobie <aberayostina@gmail.com>
2026-03-26 14:36:17 +0300

    Time: 3 ms (7.61%), Space: 19.6 MB (20.73%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,23 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        subset,curset=[],[]
+        n=len(nums)
+
+        def subsets(i,curset):
+            if i>=len(nums):
+                subset.append(curset.copy())
+                return
+
+
+            curset.append(nums[i])
+            subsets(i+1,curset)
+
+
+            curset.pop()
+            print(curset)
+
+            subsets(i+1,curset)
+            return
+
+        subsets(0,curset)
+        return subset
