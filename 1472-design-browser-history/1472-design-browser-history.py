54e829ea58da8f06b10d3bc3e5f32431cd5643fb
Yostina Abera Gobie <yostina.ugr-7272-17@aau.edu.et>
2026-05-10 13:23:50 +0300

    Time: 24 ms (80.09%), Space: 21.8 MB (88.21%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -1,23 +1,22 @@
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.f=[homepage]
+        self.head=[homepage]
         self.x=0
+        
 
     def visit(self, url: str) -> None:
-        self.f=self.f[:self.x+1]
-        self.f.append(url)
+        self.head=self.head[:self.x+1]
+        self.head.append(url)
         self.x+=1
 
 
     def back(self, steps: int) -> str:
         self.x=max(0,self.x-steps)
-        return self.f[self.x]
-
+        return self.head[self.x]
     def forward(self, steps: int) -> str:
-        self.x=min(len(self.f)-1,self.x+steps)
-        return self.f[self.x]
-
+        self.x=min(len(self.head)-1,self.x+steps)
+        return self.head[self.x]
 
 # Your BrowserHistory object will be instantiated and called as such:
 # obj = BrowserHistory(homepage)
