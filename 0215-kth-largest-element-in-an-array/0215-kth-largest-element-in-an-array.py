97883dd0cf62c46ee97e5a2be1161748aea891b9
Yostina Abera Gobie <yostina.ugr-7272-17@aau.edu.et>
2026-05-13 19:08:55 +0300

    Time: 81 ms (61.1%), Space: 29.8 MB (94.33%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,19 +1,10 @@
 class Solution:
     def findKthLargest(self, nums, k):
-
-        offset = 10000
-        size = 20001
-
-        count = [0] * size
-
-        # build frequency
-        for num in nums:
-            count[num + offset] += 1
-
-        # scan from largest to smallest
-        for i in range(size - 1, -1, -1):
-
-            k -= count[i]
-
-            if k <= 0:
-                return i - offset
+        n=len(nums)
+        from heapq import heapify, heappop
+       
+        heapify(nums)
+        for i in range(n-k):
+            heappop(nums)
+            
+        return heappop(nums)
