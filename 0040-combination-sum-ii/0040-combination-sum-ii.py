b0e496cddf5dd91716703e5699c2e16a8b4210a0
Yostina Abera Gobie <yostina.ugr-7272-17@aau.edu.et>
2026-08-03 12:26:06 +0300

    Time: 22 ms (41.52%), Space: 19.4 MB (90.31%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        result=[]
+        candidates.sort()
+        used=set()
+        def dfs(i,temp,cursum):
+            # base case
+            if cursum==target:
+                result.append(temp[:])
+                return
+
+            if cursum>target:
+                return
+
+            for j in range(i,len(candidates)):
+                # if cursum + candidates[j] <=target and candidates[j] not in used :
+                    # break
+                # else:
+                if j>i and candidates[j]==candidates[j-1]:
+                    continue
+                    
+
+                temp.append(candidates[j])
+                # used.add(candidates[j])
+                dfs(j+1,temp, cursum + candidates[j])
+                temp.pop()
+                    # used.remove(candidates[j])
+        dfs(0,[],0)
+
+
+        return result
