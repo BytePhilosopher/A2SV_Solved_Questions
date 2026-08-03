38ff6472ad0f3f0b49f20c7e123641797da849af
Yostina Abera Gobie <yostina.ugr-7272-17@aau.edu.et>
2026-08-03 12:30:53 +0300

    Time: 3 ms (86.02%), Space: 19.6 MB (13.27%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -13,12 +13,13 @@ class Solution:
                 return
 
             for j in range(i,len(candidates)):
+                if cursum + candidates[j] >target:
+                    break
 
                 if j>i and candidates[j]==candidates[j-1]:
                     continue
 
-                if cursum + candidates[j] >target:
-                    break
+
                 temp.append(candidates[j])
 
                 dfs(j+1,temp, cursum + candidates[j])
