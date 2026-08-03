a708ea8cdd076cf8d3748e372d25ad7d312139d6
Yostina Abera Gobie <yostina.ugr-7272-17@aau.edu.et>
2026-08-03 12:30:00 +0300

    Time: 1 ms (91.23%), Space: 19.6 MB (13.27%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -2,7 +2,7 @@ class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         result=[]
         candidates.sort()
-        used=set()
+ 
         def dfs(i,temp,cursum):
             # base case
             if cursum==target:
@@ -13,19 +13,17 @@ class Solution:
                 return
 
             for j in range(i,len(candidates)):
-                # if cursum + candidates[j] <=target and candidates[j] not in used :
-                    # break
-                # else:
+
                 if j>i and candidates[j]==candidates[j-1]:
                     continue
 
                 if cursum + candidates[j] >target:
                     break
                 temp.append(candidates[j])
-                # used.add(candidates[j])
+
                 dfs(j+1,temp, cursum + candidates[j])
                 temp.pop()
-                    # used.remove(candidates[j])
+
         dfs(0,[],0)
 
 
