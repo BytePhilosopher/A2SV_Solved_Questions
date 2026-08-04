13756dab7da3646a863cb5c88b6a512465478867
Yostina Abera Gobie <yostina.ugr-7272-17@aau.edu.et>
2026-08-04 13:38:57 +0300

    Time: 0 ms (100%), Space: 19.5 MB (9.24%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,17 +2,24 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         hash={"2":["a","b","c"],"3":["d","e","f"],"4":["g","h","i"],"5":["j","k","l"],
               "6":["m","n","o"], "7":["p","q","r","s"],"8":["t","u","v"],"9":["w","x","y","z"]}
-        ans=[]
-        def dfs(cur,i):
-            if i==len(digits):
-                ans.append("".join(cur))
+
+        result=[]
+
+        def dfs(i,j,temp):
+            #base case
+            if len(digits)==len(temp):
+                result.append("".join(temp))
                 return
 
+            if i>=len(digits) or j>=len(hash[digits[i]]):
+                return
+            
+            temp.append(hash[digits[i]][j])
+            dfs(i+1,0,temp)
+            temp.pop()
 
-            for j in range(len(hash[digits[i]])):
-                cur.append(hash[digits[i]][j])
-                print(cur)
-                dfs(cur,i+1)
-                cur.pop()
-        dfs([],0)
-        return ans
+            dfs(i,j+1,temp)
+
+            
+        dfs(0,0,[])
+        return result
