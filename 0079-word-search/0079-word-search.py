5b0f536ca4697584bfd156b2652220a1ba72967e
Yostina Abera Gobie <yostina.ugr-7272-17@aau.edu.et>
2026-08-05 17:35:27 +0300

    Time: 3803 ms (51.66%), Space: 19.4 MB (57.42%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,41 +1,40 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+
         if len(word)> len(board) * len(board[0]):
             return False
 
-        count=Counter([c for b in board for c in b])
-        cnt=Counter(word)
-        print(count)
+        word_count=Counter(word)
+        board_count=Counter([word for w in board for word in w])
 
-        for ch, c in cnt.items():
-            if cnt[ch]> count[ch]:
+        for ch in word_count.items():
+            if word_count[ch]> board_count[ch]:
                 return False
 
-        if count[word[0]]> count[word[-1]]:
-            word= word[::-1]
+        def dfs(row, col,ind):
 
-        def dfs(i,j,ind):
-            #if we get
             if ind==len(word):
                 return True
-            if (i<0 or i>=len(board)) or (j<0 or j>=len(board[0])) or board[i][j]!=word[ind]:
+
+            if row<0 or col<0 or row >=len(board) or col>=len(board[0]) or board[row][col]!=word[ind]:
                 return False
 
-            temp=board[i][j]
-            board[i][j]="#"
+            temp=board[row][col]
+            board[row][col]="#"
 
-            found=( dfs(i+1,j,ind+1) or 
-               dfs(i,j-1,ind+1) or 
-               dfs(i,j+1,ind+1) or 
-               dfs(i-1,j,ind+1))
-            
+            found=(dfs(row,col+1,ind+1) or
+                   dfs(row+1,col,ind+1) or
+                   dfs(row-1,col, ind+1) or
+                   dfs(row,col-1,ind+1))
 
-            #backtrack
-            board[i][j]=temp
+            board[row][col]=temp
             return found
 
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if dfs(i, j, 0):
+
+
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if dfs(r,c,0):
                     return True
+
         return False
