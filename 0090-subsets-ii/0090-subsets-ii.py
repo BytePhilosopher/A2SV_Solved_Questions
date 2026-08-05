69a4ad2dd6867a078b33a57e1fc097b8272b919c
Yostina Abera Gobie <yostina.ugr-7272-17@aau.edu.et>
2026-08-05 21:46:46 +0300

    Time: 0 ms (100%), Space: 19.4 MB (59.07%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,32 +1,22 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        path=[]
-        result=[]
         nums.sort()
-        seen=set()
+        result,curset=[],[]
 
-        def dfs(start):
+        def dfs(index):
 
             #base case
             
-            result.append(path[:])
-           
+            result.append(curset[:])
 
-            
-            # print(path)
-            for index in range(start,len(nums)):
-                if index>0 and nums[index] not in seen and nums[index]==nums[index-1]:
+            for i in range(index,len(nums)):
+                if i>index and nums[i]==nums[i-1]:
                     continue
-                path.append(nums[index])
-                seen.add(nums[index])
-                dfs(index+1)
-                seen.discard(nums[index])
-                path.pop()
+                curset.append(nums[i])
+                dfs(i+1)
+                curset.pop()
                 
 
-            
 
         dfs(0)
-   
         return result
-
