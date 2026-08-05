7815fd6da9014b5b34296d7c8955343eab8f9edb
Yostina Abera Gobie <yostina.ugr-7272-17@aau.edu.et>
2026-08-05 09:53:39 +0300

    Time: 288 ms (5.03%), Space: 37.2 MB (5.01%) - LeetHub

--- 0089-gray-code/0089-gray-code.py
+++ 0089-gray-code/0089-gray-code.py
@@ -1,34 +1,26 @@
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        # total= 2**n
 
-        # path=[0]
-        # visited={0}
+        result =[]
+        seen = set()
+        def backtrack(current):
 
-        # def onebit(a,b):
-        #     x= a ^ b
+            if current in seen:
+                
+                return
+            result.append(current)
+            seen.add(current)
+            
+            for i in range(n):
+               current = current ^ (1 << i)
+               
+               backtrack(current)
+            
+               current = current ^ (1 <<i )
+                  
+               
+        backtrack(0)
+        # for i in range(n):
+        #     print(i^(i>>n))
+        return result
 
-        #     return x and (x & (x - 1)) == 0
-
-
-        # def dfs():
-        #     if len(path)== total:
-        #         return onebit(path[0],path[-1])
-
-        #     for num in range(total):
-        #         if num not in visited and onebit(path[-1],num):
-        #             visited.add(num)
-        #             path.append(num)
-
-        #             if dfs():
-        #                 return True
-
-        #             path.pop()
-        #             visited.remove(num)
-
-        #     return False
-        
-        # dfs()
-        # return path
-        
-        return [i ^ (i>>1) for i in range(1<<n)]
